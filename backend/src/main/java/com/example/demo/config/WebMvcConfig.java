7544aa85fd19b9cfc969d76922ab5479c655a31d
hseokwoo <hseokwoo@amazon.com>
2023-05-25 22:10:49 +0900

    3rd commit

--- backend/src/main/java/com/example/demo/config/WebMvcConfig.java
+++ backend/src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -9,12 +9,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	private final long MAX_AGE_SECS = 3600;
 
 	@Override
+	/*
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000")
+						.allowedOrigins("http://localhost:3000", "http://k8s-gamedaygroupfront-33446e889b-1747407836.ap-northeast-1.elb.amazonaws.com")
 						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true)
 						.maxAge(MAX_AGE_SECS);
+	}
+	 */
+	
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").maxAge(MAX_AGE_SECS).allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
 	}
 }
